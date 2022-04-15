cf562c73e7f4643f5119c0afa04267cbe2f4c9fb
sridhar2903 <saisridhard@gmail.com>
2022-04-15 18:50:04 +0530

    backend

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();  // creating server
 app.use(cors());
 app.use("/api/v1", router);
 
-app.listen(8000, ()=>{
+app.listen(process.env.PORT || 8000, ()=>{
     console.log("Application has started successfully");
 })
 
